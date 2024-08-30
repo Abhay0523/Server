0f503f3d60805ccc46e8d0224b98ba876e080e10
Abhay <Abhay.Saxena@ambak.com>
2024-08-30 11:39:34 +0530

    new branch omvc

---
+++ Utils/Validation.js
@@ -0,0 +1,9 @@
+const checkAlphabet = /^[a-zA-Z\s]+$/;
+const checkNumber = /^[6-9]\d*$/;
+const checkStartingDigit = /^[6-9]\d*$/;
+
+const validator =(...values )=>{
+    
+}
+
+module.exports={checkAlphabet,checkNumber,checkStartingDigit};
